72bea752968bd6bef687d3100acdc395f64e6752
anuj8848 <anujgiri8848@gmail.com>
2024-05-09 23:43:57 +0545

    10th day, recursion,factorial

---
+++ recursion.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func factorial(a int) int{
+    if a==0{
+        return 1
+    }
+    return a*factorial(a-1)
+}
+
+func main(){
+    fmt.Println(factorial(7))
+    
+
+}
+
