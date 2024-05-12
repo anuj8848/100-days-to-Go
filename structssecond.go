65f6e424ed023882916e7a98db51ae1471e93895
anuj8848 <anujgiri8848@gmail.com>
2024-05-12 23:54:46 +0545

    13th day, playing with structs again

--- structssecond.go
+++ structssecond.go
@@ -9,9 +9,9 @@ type animal struct{
 
 func newAnimal(name string,legs int, sound string) *animal{
 
-	a:=animal{name: name,legs:legs,sound:sound}
-	// a.legs=legs
-	// a.sound=sound
+	a:=animal{name: name}
+	a.legs=legs
+	a.sound=sound
 	return &a
 }
 
@@ -21,13 +21,11 @@ func pl(a ...interface{}){
 
 func main(){
 
-	pl(animal{"cow",4,"vau vau"})
+	pl(animal{"cow",4,"baaaa"})
 
-	// dog:=animal()
-	// dog.name="dog"
-	// dog.legs=4
-	// dog.sound="vau vau"
+	dog:=animal{"dog",4,"vau vau"}
 
-	// pl(dog)
+	pl(dog.legs)
+
 
 }
